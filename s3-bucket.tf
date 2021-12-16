072d41f49fd36ca30a059732cf63cfa396c229ca
Mailgod <steve@steveco.net>
2021-12-16 21:20:23 +0000

    Added s3 bucket module correctly

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/MailgodTerraformTraining/s3-bucket/aws"
   version = "2.2.0"
-  # insert required variables here
+  bucket_prefix = "steveclines"
 }
 
-bucket_prefix = "steveclines"
